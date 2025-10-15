e129403961f18c00be6b0f87475761354bac3a48
Ramsiii <ramsey.rimkeit@gmail.com>
2025-10-14 17:22:29 -0700

    Added project_notes.txt to brainstorm solutions and steps in the problem.

--- queries/rentalpatterns.sql
+++ queries/rentalpatterns.sql
@@ -1,5 +1,16 @@
+-- Top 10 customers by number of rentals from the rental table
+
 SELECT customer_id, COUNT(*) as rental_count
 FROM rental 
 GROUP BY customer_id 
 ORDER BY rental_count DESC 
+LIMIT 10;
+
+-- Top 10 customers from the customer table
+
+SELECT c.customer_id, c.first_name, c.last_name, COUNT(r.rental_id) AS rental_count
+FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+GROUP BY c.customer_id
+ORDER BY rental_count DESC
 LIMIT 10;
