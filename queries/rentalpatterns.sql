504039536738750fcd537633662d1afbc31c0349
Ramsiii <ramsey.rimkeit@gmail.com>
2025-11-11 13:12:12 -0800

    Commented out faulty, incomplete SQL queries.

--- queries/rentalpatterns.sql
+++ queries/rentalpatterns.sql
@@ -1,10 +1,10 @@
 -- Top 10 customers by number of rentals from the rental table
 
-SELECT customer_id, COUNT(*) as rental_count
-FROM rental 
-GROUP BY customer_id 
-ORDER BY rental_count DESC 
-LIMIT 10;
+-- SELECT customer_id, COUNT(*) as rental_count
+-- FROM rental 
+-- GROUP BY customer_id 
+-- ORDER BY rental_count DESC 
+-- LIMIT 10;
 
 -- Top 10 customers from the customer table
 
@@ -14,3 +14,9 @@ JOIN rental r ON c.customer_id = r.customer_id
 GROUP BY c.customer_id
 ORDER BY rental_count DESC
 LIMIT 10;
+
+
+-- SELECT r.rental_id, f.film_id, f.title, r.rental_date, get_time_period(r.rental_date) as time_period 
+-- FROM rental r
+-- JOIN inventory i ON r.inventory_id = i.inventory_id
+-- JOIN film f ON i.film_id = f.film_id
