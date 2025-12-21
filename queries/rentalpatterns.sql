695ff3ce53c8b434666a8c317d036c80002703f7
Ramsiii <ramsey.rimkeit@gmail.com>
2025-12-20 17:07:11 -0800

    Fixed order, removed customer_id as not necessary for this project.

--- queries/rentalpatterns.sql
+++ queries/rentalpatterns.sql
@@ -1,3 +1,18 @@
+-- In order to start from scratch, DROP tables and restart
+
+-- DROP TABLE IF EXISTS summary_popular_films CASCADE;
+-- DROP TABLE IF EXISTS detailed_rentals CASCADE;
+
+-- get_time_period Function to replace timestamp with Quarter time period
+
+CREATE OR REPLACE FUNCTION get_time_period(rental_timestamp TIMESTAMP)
+RETURNS VARCHAR AS $$
+BEGIN
+    RETURN TO_CHAR(rental_timestamp, 'YYYY-"Q"Q');
+    -- Returns format: '2005-Q1', '2005-Q2', etc.
+END;
+$$ LANGUAGE plpgsql;
+
 -- Create detailed table of rentals:
 
 CREATE TABLE IF NOT EXISTS detailed_rentals (
@@ -5,8 +20,17 @@ CREATE TABLE IF NOT EXISTS detailed_rentals (
     film_id INTEGER NOT NULL,
     title VARCHAR(255) NOT NULL,
     rental_date TIMESTAMP NOT NULL,
+    time_period VARCHAR(10) NOT NULL
+);
+
+-- Create Summary table of popular films
+
+CREATE TABLE IF NOT EXISTS summary_popular_films (
+    film_id INTEGER NOT NULL,
+    title VARCHAR(255) NOT NULL,
     time_period VARCHAR(10) NOT NULL,
-    customer_id INTEGER NOT NULL
+    rental_count INTEGER DEFAULT 0,
+    PRIMARY KEY (film_id, time_period)
 );
 
 --populate detailed_rentals Table
@@ -17,22 +41,11 @@ SELECT
     f.film_id,
     f.title,
     r.rental_date,
-    get_time_period(r.rental_date) as time_period,
-    r.customer_id
+    get_time_period(r.rental_date) as time_period
 FROM rental r
 JOIN inventory i ON r.inventory_id = i.inventory_id
 JOIN film f ON i.film_id = f.film_id
 ON CONFLICT (rental_id) DO NOTHING;
-
--- Create Summary table of popular films
-
-CREATE TABLE  IF NOT EXISTS summary_popular_films (
-    film_id INTEGER NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    time_period VARCHAR(10) NOT NULL,
-    rental_count INTEGER DEFAULT 0,
-    PRIMARY KEY (film_id, time_period)
-);
 
 -- populate Summary table
 
@@ -46,18 +59,9 @@ FROM detailed_rentals
 GROUP BY film_id, title, time_period
 ON CONFLICT (film_id, time_period) DO NOTHING;
 
--- get_time_period Function to replace timestamp with Quarter time period
-
-CREATE OR REPLACE FUNCTION get_time_period(rental_timestamp TIMESTAMP)
-RETURNS VARCHAR AS $$
-BEGIN
-    RETURN TO_CHAR(rental_timestamp, 'YYYY-"Q"Q');
-    -- Returns format: '2005-Q1', '2005-Q2', etc.
-END;
-$$ LANGUAGE plpgsql;
-
 -- Trigger to recalculate summary table:
 
+-- TODO: Create Trigger
 
 -- Detailed query to display data which populates detailed_rentals
 
@@ -65,3 +69,4 @@ SELECT r.rental_id, f.film_id, f.title, r.rental_date, get_time_period(r.rental_
 FROM rental r
 JOIN inventory i ON r.inventory_id = i.inventory_id
 JOIN film f ON i.film_id = f.film_id;
+
