b33323caf4e0505d853c3a45d38b7f2e7fe6ef44
Ramsiii <ramsey.rimkeit@gmail.com>
2025-12-20 16:07:54 -0800

    Completely recreated most of the necessary queries for rentalpatterns.sql

--- queries/rentalpatterns.sql
+++ queries/rentalpatterns.sql
@@ -1,3 +1,53 @@
+-- Create detailed table of rentals:
+
+CREATE TABLE IF NOT EXISTS detailed_rentals (
+    rental_id INTEGER PRIMARY KEY,
+    film_id INTEGER NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    rental_date TIMESTAMP NOT NULL,
+    time_period VARCHAR(10) NOT NULL,
+    customer_id INTEGER NOT NULL
+);
+
+--populate detailed_rentals Table
+
+INSERT INTO detailed_rentals
+SELECT 
+    r.rental_id,
+    f.film_id,
+    f.title,
+    r.rental_date,
+    get_time_period(r.rental_date) as time_period,
+    r.customer_id
+FROM rental r
+JOIN inventory i ON r.inventory_id = i.inventory_id
+JOIN film f ON i.film_id = f.film_id
+ON CONFLICT (rental_id) DO NOTHING;
+
+-- Create Summary table of popular films
+
+CREATE TABLE  IF NOT EXISTS summary_popular_films (
+    film_id INTEGER NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    time_period VARCHAR(10) NOT NULL,
+    rental_count INTEGER DEFAULT 0,
+    PRIMARY KEY (film_id, time_period)
+);
+
+-- populate Summary table
+
+INSERT INTO summary_popular_films (film_id, title, time_period, rental_count)
+SELECT 
+    film_id,
+    title,
+    time_period,
+    COUNT(*) as rental_count
+FROM detailed_rentals
+GROUP BY film_id, title, time_period
+ON CONFLICT (film_id, time_period) DO NOTHING;
+
+-- get_time_period Function to replace timestamp with Quarter time period
+
 CREATE OR REPLACE FUNCTION get_time_period(rental_timestamp TIMESTAMP)
 RETURNS VARCHAR AS $$
 BEGIN
@@ -9,13 +59,9 @@ $$ LANGUAGE plpgsql;
 -- Trigger to recalculate summary table:
 
 
+-- Detailed query to display data which populates detailed_rentals
 
--- Part D: Raw Data:
 SELECT r.rental_id, f.film_id, f.title, r.rental_date, get_time_period(r.rental_date) as time_period 
 FROM rental r
 JOIN inventory i ON r.inventory_id = i.inventory_id
-JOIN film f ON i.film_id = f.film_id
- 
- -- test the first again
- -- tested
- -- add function and trigger to update most rented
+JOIN film f ON i.film_id = f.film_id;
