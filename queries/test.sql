86ee417bb3ca878a4887ccc4b6f8d27bbfafc29f
Ramsiii <ramsey.rimkeit@gmail.com>
2025-12-30 14:06:43 -0800

    Created a new test to add inventory to rentaldvd and demonstrate the efficacy of the Trigger and the Procedure. Moved the DELETE statement test to test.sql.

--- queries/test.sql
+++ queries/test.sql
@@ -1,3 +1,5 @@
+-- COMMENT OUT EACH STEP UNTIL READY --
+
 -- List all existing Triggers:
 
 -- SELECT * FROM pg_trigger WHERE tgname LIKE 'trg_%';
@@ -16,3 +18,32 @@
 -- List all existing Triggers:
 
 -- SELECT * FROM pg_trigger WHERE tgname LIKE 'trg_%';
+
+
+
+-- Test PROCEDURE with DELETE statements --
+
+-- 1a. Show current state of detailed table
+-- SELECT COUNT(*) FROM detailed_rentals;  -- e.g., 16,044 rows
+
+-- 1b. Show current state of summary table
+-- SELECT COUNT(*) FROM summary_popular_films;  -- e.g., 100 rows
+
+-- 2. Manually corrupt/delete data
+-- DELETE FROM detailed_rentals WHERE rental_id < 100;
+
+-- DELETE FROM summary_popular_films WHERE film_id < 100;
+
+-- 3. Show damaged state
+-- SELECT COUNT(*) FROM detailed_rentals;  -- e.g., 15,945 rows
+
+-- SELECT COUNT(*) FROM summary_popular_films;  -- e.g., 95 rows
+
+-- 4. Run the procedure
+
+-- CALL refresh_rental_analysis();
+
+-- 5. Show restored state
+-- SELECT COUNT(*) FROM detailed_rentals;  -- Back to 16,044
+
+-- SELECT COUNT(*) FROM summary_popular_films;  -- Back to 100 rows
--- queries/test_procedure_changes.sql
+++ queries/test_procedure_changes.sql
@@ -1,31 +1,9 @@
 -- COMMENT OUT EACH STEP UNTIL READY --
 
--- 1a. Show current state of detailed table
--- SELECT COUNT(*) FROM detailed_rentals;  -- e.g., 16,044 rows
-
--- 1b. Show current state of summary table
--- SELECT COUNT(*) FROM summary_popular_films;  -- e.g., 100 rows
-
--- 2. Manually corrupt/delete data
--- DELETE FROM detailed_rentals WHERE rental_id < 100;
-
--- DELETE FROM summary_popular_films WHERE film_id < 100;
-
--- 3. Show damaged state
--- SELECT COUNT(*) FROM detailed_rentals;  -- e.g., 15,945 rows
-
--- SELECT COUNT(*) FROM summary_popular_films;  -- e.g., 95 rows
-
--- 4. Run the procedure
-
--- CALL refresh_rental_analysis();
-
--- 5. Show restored state
--- SELECT COUNT(*) FROM detailed_rentals;  -- Back to 16,044
-
--- SELECT COUNT(*) FROM summary_popular_films;  -- Back to 100 rows
-
 -- CHANGE RENTAL INVENTORY --
+
+-- Check summary table
+-- SELECT * FROM summary_popular_films ORDER BY popularity_rank LIMIT 5;
 
 -- Find available inventory for "Rocketeer Mother"
 -- SELECT inventory_id FROM inventory WHERE film_id = 738 LIMIT 2; -- inventory_d for film_id 738 = 3360 & 3361
