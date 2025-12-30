5975e3a8a7909d1e60a7c0d1f39d2da51444fa7e
Ramsiii <ramsey.rimkeit@gmail.com>
2025-12-30 14:15:29 -0800

    Fixed spacing error and commented in the first statement in test_procedure_changes.sql

--- queries/test_procedure_changes.sql
+++ queries/test_procedure_changes.sql
@@ -3,10 +3,14 @@
 -- CHANGE RENTAL INVENTORY --
 
 -- Check summary table
--- SELECT * FROM summary_popular_films ORDER BY popularity_rank LIMIT 5;
+SELECT * FROM summary_popular_films ORDER BY popularity_rank LIMIT 5;
 
--- Find available inventory for "Rocketeer Mother"
--- SELECT inventory_id FROM inventory WHERE film_id = 738 LIMIT 2; -- inventory_d for film_id 738 = 3360 & 3361
+-- Should display:
+-- 103 "Bucket Brotherhood" 34 1
+-- 738 "Rocketeer Mother" 33 2
+
+-- Find available inventory ids for "Rocketeer Mother"
+-- SELECT inventory_id FROM inventory WHERE film_id = 738 LIMIT 2; -- inventory_id for film_id 738 = 3360 & 3361
 -- SELECT MAX(rental_id) FROM rental; -- max rental_id = 16049
 
 -- INSERT INTO rental (rental_date, inventory_id, customer_id, staff_id, return_date)
